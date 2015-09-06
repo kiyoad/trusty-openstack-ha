3de1ab9822f12539529c673ecba9d0d4c49bb317
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-09-06 11:01:28 +0900

    Remove unused role

--- ansible/roles/corosync-pacemaker-starter/files/reset-cib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-ssh sv1 sudo service corosync stop
-ssh sv2 sudo service corosync stop
-ssh sv3 sudo service corosync stop
-ssh sv1 sudo rm /var/lib/pacemaker/cib/*
-ssh sv2 sudo rm /var/lib/pacemaker/cib/*
-ssh sv3 sudo rm /var/lib/pacemaker/cib/*
